d9a95c2848ce219851f887b24cd73a54195cae02
Kim Borup <kborup@redhat.com>
2016-06-01 21:21:08 +0200

    changed in variables

--- ansibleinst.sh
+++ ansibleinst.sh
@@ -29,7 +29,7 @@ osm_use_cockpit=false
 
 
 # default subdomain to use for exposed routes
-openshift_master_default_subdomain=${DOMAIN}
+openshift_master_default_subdomain=${DNSOPT}
 
 # default project node selector
 osm_default_node_selector='env=dev'
@@ -56,23 +56,23 @@ openshift_hosted_router_replicas=1
 openshift_registry_selector='region=infra'
 
 # Configure metricsPublicURL in the master config for cluster metrics
-openshift_master_metrics_public_url=https://hawkular-metrics.${DOMAIN}
+openshift_master_metrics_public_url=https://hawkular-metrics.${DNSOPT}
 
 # Configure loggingPublicURL in the master config for aggregate logging
-openshift_master_logging_public_url=https://kibana.${DOMAIN}
+openshift_master_logging_public_url=https://kibana.${DNSOPT}
 
 # host group for masters
 [masters]
-master00.${DOMAIN}
+master00.${DNSOPT}
 # host group for etcd
 [etcd]
-master00.${DOMAIN}
+master00.${DNSOPT}
 
 # host group for nodes, includes region info
 [nodes]
-master00.${DOMAIN} openshift_public_hostname="master00.${DOMAIN}" " openshift_schedulable=False openshift_node_labels="{'name': 'master00'}"
-infranode00.${DOMAIN}  openshift_public_hostname="infranode00.${DOMAIN}" " openshift_node_labels="{'name': 'infranode00','region': '${REGION}', 'zone': '${AZ1}', 'region': 'infra'}"
-node00.${DOMAIN}  openshift_node_labels="{'name': 'node00','region': '${REGION}', 'zone': '${AZ1}', 'env': 'dev'}"
-node01.${DOMAIN}  openshift_node_labels="{'name': 'node01','region': '${REGION}', 'zone': '${AZ1}', 'env': 'dev'}"
+master00.${DNSOPT} openshift_public_hostname="master00.${DNSOPT}" " openshift_schedulable=False openshift_node_labels="{'name': 'master00'}"
+infranode00.${DNSOPT}  openshift_public_hostname="infranode00.${DNSOPT}" " openshift_node_labels="{'name': 'infranode00','region': '${AWSREGION}', 'zone': '${AZ1}', 'region': 'infra'}"
+node00.${DNSOPT}  openshift_node_labels="{'name': 'node00','region': '${AWSREGION}', 'zone': '${AZ1}', 'env': 'dev'}"
+node01.${DNSOPT}  openshift_node_labels="{'name': 'node01','region': '${AWSREGION}', 'zone': '${AZ1}', 'env': 'dev'}"
 
 EOF
