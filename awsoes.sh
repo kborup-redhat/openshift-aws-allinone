c28642ba4628cae0c3d1255f30bd87dd505dda62
Kim Borup <kborup@redhat.com>
2016-06-29 19:38:17 +0200

    Changed help and settings

--- awsoes.sh
+++ awsoes.sh
@@ -14,26 +14,8 @@ Create an AWS environment running openshift 3.2 under RHEL 7.2
 
 EXAMPLE:
 $script_name --rhuser <username> --rhpool <poolid> --cluster true / false> --awsrhid <rhelImageId> --awsregion <Aws region> --dnsopt=dnsname --awsdnszone <ZONEID> --awsdns= true/false
-OPTIONS EXPLAINED: 
-rhuser = your redhat user it
-rhpass = your redhat password 
-rhpool = from from "subscription-manager list --avalibale --all" (take the repo with openshift in it)
-cluster = do you want a clustered AWS setup with loadbalancer, multiple infranodes and routes then this is the option for you (currently under construction)
-awsrhid = this is the ID from your AWS console LAUNCH Instance you can pick Red Hat Enterprise Linux 7.2 (HVM), SSD Volume Type - ami-775e4f16 (take the ami- number and insert here)
-awsregion = use one of the regions listed in amazon. aws ec2 describe-regions will list something like: 
-dnsopt = dnsname of router and servers could be cloud.pfy.dk cloud.redhat.com or cloud.google.com depending on what you own.
 
-#REGIONS	ec2.eu-west-1.amazonaws.com	eu-west-1
-#REGIONS	ec2.ap-southeast-1.amazonaws.com	ap-southeast-1
-#REGIONS	ec2.ap-southeast-2.amazonaws.com	ap-southeast-2
-#REGIONS	ec2.eu-central-1.amazonaws.com	eu-central-1
-#REGIONS	ec2.ap-northeast-2.amazonaws.com	ap-northeast-2
-#REGIONS	ec2.ap-northeast-1.amazonaws.com	ap-northeast-1
-#REGIONS	ec2.us-east-1.amazonaws.com	us-east-1
-#REGIONS	ec2.sa-east-1.amazonaws.com	sa-east-1
-#REGIONS	ec2.us-west-1.amazonaws.com	us-west-1
-#REGIONS	ec2.us-west-2.amazonaws.com	us-west-2
-
+Read the README.md file for explernations
 
 EOF
 exit 1
@@ -79,6 +61,13 @@ EOF
 exit 1
 }
 
+awsdnsmiss ()
+{
+cat << EOF
+You are missing your AWS DNS ZONE ID NAME, read README.md
+EOF
+exit 1
+}
 
 OPTIONS=`getopt -o h -l help -l rhuser: -l rhpool: -l cluster: -l clusterid: -l awsrhid: -l awsregion: -l dnsopt: -l awsdns: -l awsdnszone: -- "$@"`
 
@@ -117,7 +106,7 @@ esac
 done
 if [ $AWSDNS == true ]; then
 if [ -z "$AWSDNSZONE" ]; then
-usage
+awsdnsmiss
 fi
 fi 
 
@@ -125,16 +114,13 @@ if [ ! -f /usr/local/bin/aws ]; then
 	awsmiss
 fi
 
-if [ -z "$RHUSER" ]; then
-        usage
-fi
 
 echo "Please enter you password for RHN";
 stty -echo
 read RHPASS;
 stty echo
 
-if [ -z "$RHPOOL|$CLUSTER|$CLUSTERID|$AWSRHID|$AWSREGION|$DNSOPT|$AWSDNS" ]; then 
+if [ -z "$RHUSER|$RHPOOL|$CLUSTER|$CLUSTERID|$AWSRHID|$AWSREGION|$DNSOPT|$AWSDNS" ]; then 
 	usage
 fi
 
@@ -152,6 +138,8 @@ else
 	aws ec2 create-key-pair --key-name ${KEYNAME} --query 'KeyMaterial' --output text > ~/.ssh/${KEYNAME}.pem
 	chmod 600 ~/.ssh/${KEYNAME}.pem
 fi
+
+RS="ssh -ti ~/.ssh/${KEYNAME}.pem -l ec2-user"
 
 echo "Creating VPC Network"
 VPCID=`aws ec2 create-vpc --cidr-block 192.168.0.0/24 --output text | awk '{print $7}'`
@@ -295,9 +283,9 @@ SHORTLAB00PUBLICDNS=lab.${DNSOPT}
 LAB00PUBLICIP=`aws ec2 describe-instances --instance-ids $LAB00ID --query Reservations[*].Instances[*].[PublicIpAddress] --output text`
 LAB00PRIVATEIP=`aws ec2 describe-instances --instance-ids $LAB00ID --query Reservations[*].Instances[*].[PrivateIpAddress] --output text`
 echo "sleeping while we wait for node to become ready its in the cloud!!"
-sleep 3m
+sleep 2m
 
-ssh -ti ~/.ssh/${KEYNAME}.pem ec2-user@${LAB00PUBLICIP} "
+$RS ${LAB00PUBLICIP} "
 sudo sudo rm  /etc/yum.repos.d/*
 sudo subscription-manager register --username=${RHUSER} --password=${RHPASS}
 sudo subscription-manager attach --pool=${RHPOOL}
@@ -366,7 +354,7 @@ NFS00PUBLICIP=`aws ec2 describe-instances --instance-ids $NFSNODE00ID --query Re
 aws ec2 create-tags --resource $NFSNODE00ID --tags Key=deployment,Value=paas Key=type,Value=instance Key=Name,Value=${CLUSTERID}_nfsnode00 Key=clusterid,Value=${CLUSTERID}
 NFSVOL=`aws ec2 create-volume --region $AWSREGION --availability-zone $AZ1 --size 20 --volume-type gp2 | awk '{print $7}'`
 echo "sleeping 4 minutts waiting for the disk to be ready"
-sleep 4m 
+sleep 2m 
 aws ec2 attach-volume --volume-id $NFSVOL --instance-id $NFSNODE00ID --device /dev/sdb 
 
 if [ $CLUSTER = true ]; then 
@@ -378,7 +366,7 @@ echo "Setting up RHN on nodes"
 nodes="${MASTER00PUBLICDNS} ${INFRANODE00PUBLICDNS} ${NODE00PUBLICDNS} ${NODE01PUBLICDNS}"
 fi 
 
-for node in $nodes ; do ssh -ti ~/.ssh/${KEYNAME}.pem ec2-user@${node} "
+for node in $nodes ; do $RS ${node} "
 echo Configure the Repositories on ${node} if any of this looks to hang press ENTER
 sudo sudo rm  /etc/yum.repos.d/*
 sudo subscription-manager register --username=${RHUSER} --password=${RHPASS}
@@ -394,7 +382,7 @@ sudo sed -i 's/--selinux-enabled/--selinux-enabled --insecure-registry 172.30.0.
 sudo reboot
 " ; done
 echo "sleeping until all nodes are back up"
-sleep 5m
+sleep 2m
 
 
 
@@ -612,9 +600,8 @@ chmod 775 $DIR/ansible-hosts-standalone.sh
 sh $DIR/ansible-hosts-standalone.sh
 fi 
 #Adding nfs disks
-#tar cvf - nfs.setup.sh | ssh -i ~/.ssh/${KEYNAME}.pem -l ec2-user $NFS00PUBLICIP tar xvf -
 scp -i ~/.ssh/${KEYNAME}.pem $DIR/nfs.setup.sh ec2-user@$NFS00PUBLICIP:
-ssh -ti ~/.ssh/${KEYNAME}.pem -l ec2-user $NFS00PUBLICIP "sudo bash /home/ec2-user/nfs.setup.sh"
+$RS $NFS00PUBLICIP "sudo bash /home/ec2-user/nfs.setup.sh"
 scp -i ~/.ssh/${KEYNAME}.pem ~/.ssh/${KEYNAME}.pem ec2-user@$LAB00PUBLICIP:/home/ec2-user/.ssh/
 
 cat << EOF > $DIR/config
@@ -625,24 +612,23 @@ Host *
 EOF
 scp -i ~/.ssh/${KEYNAME}.pem $DIR/ansible-hosts  ec2-user@$LAB00PUBLICIP:
 scp -i ~/.ssh/${KEYNAME}.pem $DIR/config  ec2-user@$LAB00PUBLICIP:/home/ec2-user/.ssh/
-ssh -ti  ~/.ssh/${KEYNAME}.pem -l ec2-user $LAB00PUBLICIP 'chmod 600 .ssh/config'
+$RS $LAB00PUBLICIP 'chmod 600 .ssh/config'
 
-ssh -ti  ~/.ssh/${KEYNAME}.pem -l ec2-user $LAB00PUBLICIP 'sudo yum -y install atomic-openshift-utils'
+$RS $LAB00PUBLICIP 'sudo yum -y install atomic-openshift-utils'
 
-ssh -ti  ~/.ssh/${KEYNAME}.pem -l ec2-user $LAB00PUBLICIP 'echo StrictHostKeyChecking no | sudo tee -a /etc/ssh/ssh_config'
-ssh -ti  ~/.ssh/${KEYNAME}.pem -l ec2-user $LAB00PUBLICIP 'sudo cp /home/ec2-user/ansible-hosts /etc/ansible/hosts'
-ssh -ti  ~/.ssh/${KEYNAME}.pem -l ec2-user $LAB00PUBLICIP 'sudo cp /home/ec2-user/.ssh/* /root/.ssh/'
-ssh -ti  ~/.ssh/${KEYNAME}.pem -l ec2-user $LAB00PUBLICIP 'sudo chmod 600 /root/.ssh/config '
+$RS $LAB00PUBLICIP 'echo StrictHostKeyChecking no | sudo tee -a /etc/ssh/ssh_config'
+$RS $LAB00PUBLICIP 'sudo cp /home/ec2-user/ansible-hosts /etc/ansible/hosts'
+$RS $LAB00PUBLICIP 'sudo cp /home/ec2-user/.ssh/* /root/.ssh/'
+$RS $LAB00PUBLICIP 'sudo chmod 600 /root/.ssh/config '
 echo "Installing Your Environment"
-ssh -ti  ~/.ssh/${KEYNAME}.pem -l ec2-user $LAB00PUBLICIP 'sudo ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml'
+$RS $LAB00PUBLICIP 'sudo ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml'
 
 
 #Router and Registry deployment
 echo "Deploying router and registry"
 
-ssh -ti ~/.ssh/${KEYNAME}.pem -l ec2-user $MASTER00PUBLICIP "sudo oc get namespace default -o yaml > namespace.default.yaml ; sed -i  '/annotations/ a \ \ \ \ openshift.io/node-selector: region=infra' namespace.default.yaml ; oc replace -f namespace.default.yaml"
-ssh -ti ~/.ssh/${KEYNAME}.pem -l ec2-user $MASTER00PUBLICIP "sudo oadm registry --replicas=1 --create --credentials=/etc/origin/master/openshift-registry.kubeconfig --images='registry.access.redhat.com/openshift3/ose-docker-registry:latest'"
-#ssh -ti ~/.ssh/${KEYNAME}.pem -l ec2-user $MASTER00PUBLICIP "sudo oadm router router --replicas=1 -service-account=router --stats-password='awslab' --images='registry.access.redhat.com/openshift3/ose-haproxy-router:latest'"
+$RS $MASTER00PUBLICIP "sudo oc get namespace default -o yaml > namespace.default.yaml ; sed -i  '/annotations/ a \ \ \ \ openshift.io/node-selector: region=infra' namespace.default.yaml ; oc replace -f namespace.default.yaml"
+$RS $MASTER00PUBLICIP "sudo oadm registry --replicas=1 --create --credentials=/etc/origin/master/openshift-registry.kubeconfig --images='registry.access.redhat.com/openshift3/ose-docker-registry:latest'"
 
 cat << EOF > $DIR/pvconfig
 {
@@ -684,21 +670,21 @@ EOF
 
 scp -i ~/.ssh/${KEYNAME}.pem $DIR/pvclaim  ec2-user@$MASTER00PUBLICIP:
 scp -i ~/.ssh/${KEYNAME}.pem $DIR/pvconfig  ec2-user@$MASTER00PUBLICIP:
-ssh -ti ~/.ssh/${KEYNAME}.pem -l ec2-user $MASTER00PUBLICIP "sudo cat pvconfig | oc create -f -"
-ssh -ti ~/.ssh/${KEYNAME}.pem -l ec2-user $MASTER00PUBLICIP "sudo cat pvclaim | oc create -f -"
+$RS $MASTER00PUBLICIP "sudo cat pvconfig | oc create -f -"
+$RS $MASTER00PUBLICIP "sudo cat pvclaim | oc create -f -"
 
-ssh -ti ~/.ssh/${KEYNAME}.pem -l ec2-user $MASTER00PUBLICIP "sudo oc volume dc/docker-registry --add --overwrite -t persistentVolumeClaim --claim-name=registry-claim --name=registry-storage"
+$RS $MASTER00PUBLICIP "sudo oc volume dc/docker-registry --add --overwrite -t persistentVolumeClaim --claim-name=registry-claim --name=registry-storage"
 
 for node in  ${MASTER00PUBLICIP}
 do
-ssh -ti ~/.ssh/${KEYNAME}.pem ec2-user@${node} "
+$RS ${node} "
 sudo yum -y install httpd-tools
 sudo touch /etc/origin/openshift-passwd
 sudo htpasswd -b /etc/origin/openshift-passwd testuser flaf42mn
 ";
 done
 
-ssh -ti ~/.ssh/${KEYNAME}.pem -l ec2-user $MASTER00PUBLICIP "sudo oc get namespace openshift-infra -o yaml > openshift-infra.yaml ; sed -i  '/annotations/ a \ \ \ \ openshift.io/node-selector: region=infra' openshift-infra.yaml ; oc replace -f openshift-infra.yaml"
+$RS $MASTER00PUBLICIP "sudo oc get namespace openshift-infra -o yaml > openshift-infra.yaml ; sed -i  '/annotations/ a \ \ \ \ openshift.io/node-selector: region=infra' openshift-infra.yaml ; oc replace -f openshift-infra.yaml"
 echo creating logging and metrics
 
 cat << EOF > $DIR/sa.json
@@ -712,9 +698,9 @@ EOF
 
 scp -i ~/.ssh/${KEYNAME}.pem sa.json  ec2-user@$MASTER00PUBLICIP: 
 
-ssh -ti ~/.ssh/${KEYNAME}.pem -l ec2-user $MASTER00PUBLICIP "sudo oc project openshift-infra ; sudo cat /home/ec2-user/sa.json | oc create -f -" 
-ssh -ti ~/.ssh/${KEYNAME}.pem -l ec2-user $MASTER00PUBLICIP "sudo oc project openshift-infra ; sudo oadm policy add-role-to-user edit system:serviceaccount:openshift-infra:metrics-deployer"
-ssh -ti ~/.ssh/${KEYNAME}.pem -l ec2-user $MASTER00PUBLICIP "sudo oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:openshift-infra:heapster ; sudo  oc secrets new metrics-deployer nothing=/dev/null"
+$RS $MASTER00PUBLICIP "sudo oc project openshift-infra ; sudo cat /home/ec2-user/sa.json | oc create -f -" 
+$RS $MASTER00PUBLICIP "sudo oc project openshift-infra ; sudo oadm policy add-role-to-user edit system:serviceaccount:openshift-infra:metrics-deployer"
+$RS $MASTER00PUBLICIP "sudo oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:openshift-infra:heapster ; sudo  oc secrets new metrics-deployer nothing=/dev/null"
 
 cat << EOF > $DIR/metrics-vol
 {
@@ -749,8 +735,8 @@ EOF
 
 scp -i ~/.ssh/${KEYNAME}.pem $DIR/metrics-vol  ec2-user@$MASTER00PUBLICIP:
 scp -i ~/.ssh/${KEYNAME}.pem $DIR/logging-sa ec2-user@$MASTER00PUBLICIP:
-ssh -ti ~/.ssh/${KEYNAME}.pem -l ec2-user $MASTER00PUBLICIP "sudo oc project openshift-infra ; sudo cat metrics-vol | oc create -f -"
-ssh -ti ~/.ssh/${KEYNAME}.pem -l ec2-user $MASTER00PUBLICIP "sudo oc project openshift-infra ; sudo oc process -f metrics-deployer.yaml -v HAWKULAR_METRICS_HOSTNAME=hawkular-metrics.${DNSOPT},CASSANDRA_PV_SIZE=10Gi |  oc create -f -"
-ssh -ti ~/.ssh/${KEYNAME}.pem -l ec2-user $MASTER00PUBLICIP "sudo oadm new-project logging --node-selector region=infra ; sudo oc project logging ;  sudo oc secrets new logging-deployer nothing=/dev/null ; sudo cat logging-sa | oc create -f - ; sudo oc policy add-role-to-user edit system:serviceaccount:logging:logging-deployer ; sudo oadm policy add-scc-to-user privileged system:serviceaccount:logging:aggregated-logging-fluentd ; sudo oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:logging:aggregated-logging-fluentd ; sudo oc process logging-deployer-template -n openshift -v KIBANA_HOSTNAME=kibana.${DNSOPT} -v ES_CLUSTER_SIZE=1 -v ES_INSTANCE_RAM=1024M -v PUBLIC_MASTER_URL=https://master00.${DNSOPT}:8443 -v ENABLE_OPS_CLUSTER=false -v IMAGE_VERSION=latest,IMAGE_PREFIX=registry.access.redhat.com/openshift3/ | oc create -f - ; sudo oc get dc `oc get dc | grep logging-es | awk '{print $1}'` -o json | sed '/containers/i "nodeSelector": { "env": "dev" },' | oc replace -f -"
+$RS $MASTER00PUBLICIP "sudo oc project openshift-infra ; sudo cat metrics-vol | oc create -f -"
+$RS $MASTER00PUBLICIP "sudo oc project openshift-infra ; sudo oc process -f metrics-deployer.yaml -v HAWKULAR_METRICS_HOSTNAME=hawkular-metrics.${DNSOPT},CASSANDRA_PV_SIZE=10Gi |  oc create -f -"
+$RS $MASTER00PUBLICIP "sudo oadm new-project logging --node-selector region=infra ; sudo oc project logging ;  sudo oc secrets new logging-deployer nothing=/dev/null ; sudo cat logging-sa | oc create -f - ; sudo oc policy add-role-to-user edit system:serviceaccount:logging:logging-deployer ; sudo oadm policy add-scc-to-user privileged system:serviceaccount:logging:aggregated-logging-fluentd ; sudo oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:logging:aggregated-logging-fluentd ; sudo oc process logging-deployer-template -n openshift -v KIBANA_HOSTNAME=kibana.${DNSOPT} -v ES_CLUSTER_SIZE=1 -v ES_INSTANCE_RAM=1024M -v PUBLIC_MASTER_URL=https://master00.${DNSOPT}:8443 -v ENABLE_OPS_CLUSTER=false -v IMAGE_VERSION=latest,IMAGE_PREFIX=registry.access.redhat.com/openshift3/ | oc create -f - ; sudo oc get dc `oc get dc | grep logging-es | awk '{print $1}'` -o json | sed '/containers/i "nodeSelector": { "env": "dev" },' | oc replace -f -"
 
 
